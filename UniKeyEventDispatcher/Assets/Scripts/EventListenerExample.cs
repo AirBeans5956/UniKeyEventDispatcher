57e549173f008a2316ab09bad1cc925b80526ade
AirBeans <xq_shil_px@yahoo.co.jp>
2017-10-29 18:38:28 +0900

    [Add]Add sample  key event reciever

---
+++ UniKeyEventDispatcher/Assets/Scripts/EventListenerExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventListenerExample : MonoBehaviour, IKeyUpEvent, IKeyDownEvent {
+
+    Text testLabel;
+
+    void Start() {
+        testLabel = GetComponent<Text>();
+    }
+
+    public void OnKeyDown(List<KeyCode> downKeyList) {
+        string labelText = "Down key: ";
+        foreach (KeyCode key in downKeyList) {
+            labelText += key.ToString() + " ";
+        }
+        testLabel.text = labelText;
+    }
+
+    public void OnKeyUp(List<KeyCode> upKeyList) {
+        string labelText = "Up key: ";
+        foreach (KeyCode key in upKeyList) {
+            labelText += key.ToString() + " ";
+        }
+        testLabel.text = labelText;
+    }
+}
